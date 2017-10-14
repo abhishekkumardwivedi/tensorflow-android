f67dbc3111b35433c509e609555ef639fc677879
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2017-10-14 17:14:12 +0530

    working with mobile

--- tensorflow/examples/android/src/org/tensorflow/demo/ClassifierActivity.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/ClassifierActivity.java
@@ -23,22 +23,54 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.media.ImageReader.OnImageAvailableListener;
+import android.os.AsyncTask;
 import android.os.SystemClock;
+import android.util.Log;
 import android.util.Size;
 import android.util.TypedValue;
 import android.view.Display;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequest;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 import java.util.Vector;
 import org.tensorflow.demo.OverlayView.DrawCallback;
 import org.tensorflow.demo.env.BorderedText;
 import org.tensorflow.demo.env.ImageUtils;
 import org.tensorflow.demo.env.Logger;
-import org.tensorflow.demo.R; // Explicit import needed for internal Google builds.
 
 public class ClassifierActivity extends CameraActivity implements OnImageAvailableListener {
   private static final Logger LOGGER = new Logger();
+  private static final String TAG = ClassifierActivity.class.getSimpleName();
 
   protected static final boolean SAVE_PREVIEW_BITMAP = false;
+  private static final String CLOUD_VISION_API_KEY = "AIzaSyCCwJmcg7E0RfBSCBXaWLQynOur9-HxxXw";
+  private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+  private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
+  private static final int MAX_VISION_QUEUE = 5; // to reduce cloud API hit.
+  private static int inCloudVisionQueue= 0;
 
   private ResultsView resultsView;
 
@@ -70,11 +102,11 @@ public class ClassifierActivity extends CameraActivity implements OnImageAvailab
   private static final String INPUT_NAME = "input";
   private static final String OUTPUT_NAME = "output";
 
-
   private static final String MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb";
   private static final String LABEL_FILE =
       "file:///android_asset/imagenet_comp_graph_label_strings.txt";
 
+  private static Set<String> codes = new HashSet<String>();
 
   private static final boolean MAINTAIN_ASPECT = true;
 
@@ -123,6 +155,11 @@ public class ClassifierActivity extends CameraActivity implements OnImageAvailab
     previewWidth = size.getWidth();
     previewHeight = size.getHeight();
 
+    codes.addAll(Arrays.asList(new String[]
+            {"AN","AP","AR","AS","BR","CG","CH","DD","DL","DN","GA","GJ","HR","HP","JH","JK","KA",
+                    "KL","LD","MH","ML","MN","MP","MZ","NL","OD","PB","PY","RJ","SK","TN","TR","TS",
+                    "UK","UP","WB" }));
+
     final Display display = getWindowManager().getDefaultDisplay();
     final int screenOrientation = display.getRotation();
 
@@ -169,6 +206,15 @@ public class ClassifierActivity extends CameraActivity implements OnImageAvailab
             final List<Classifier.Recognition> results = classifier.recognizeImage(croppedBitmap);
             lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
             LOGGER.i("Detect: %s", results);
+            for(int i=0; i < results.size(); i++) {
+              String title = results.get(i).getTitle();
+              Log.d(TAG, title);
+              if((title.equals("car") || title.equals("sports car")) && inCloudVisionQueue < MAX_VISION_QUEUE) {
+                Log.d(TAG,"Car found");
+                cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                findLicensePlate(cropCopyBitmap);
+              }
+            }
             cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
             if (resultsView == null) {
               resultsView = (ResultsView) findViewById(R.id.results);
@@ -178,6 +224,197 @@ public class ClassifierActivity extends CameraActivity implements OnImageAvailab
             readyForNextImage();
           }
         });
+  }
+
+  public void findLicensePlate(Bitmap croppedBitmap) {
+    if (croppedBitmap != null) {
+      try {
+        // scale the image to save on bandwidth
+        Bitmap bitmap =
+                scaleBitmapDown(croppedBitmap, 1200);
+//                scaleBitmapDown(
+//                        MediaStore.Images.Media.getBitmap(getContentResolver(), uri),
+//                        1200);
+
+        callCloudVision(bitmap);
+
+      } catch (IOException e) {
+        Log.d(TAG, "Image picking failed because " + e.getMessage());
+      }
+    } else {
+      Log.d(TAG, "Image picker gave us a null image.");
+    }
+  }
+
+  private void callCloudVision(final Bitmap bitmap) throws IOException {
+    // Switch text to loading
+
+    // Do the real work in an async task, because we need to use the network anyway
+    new AsyncTask<Object, Void, String>() {
+      @Override
+      protected String doInBackground(Object... params) {
+        try {
+          HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+          JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+          VisionRequestInitializer requestInitializer =
+                  new VisionRequestInitializer(CLOUD_VISION_API_KEY) {
+                    /**
+                     * We override this so we can inject important identifying fields into the HTTP
+                     * headers. This enables use of a restricted cloud platform API key.
+                     */
+                    @Override
+                    protected void initializeVisionRequest(VisionRequest<?> visionRequest)
+                            throws IOException {
+                      super.initializeVisionRequest(visionRequest);
+
+                      String packageName = getPackageName();
+                      visionRequest.getRequestHeaders().set(ANDROID_PACKAGE_HEADER, packageName);
+
+                      String sig = PackageManagerUtils.getSignature(getPackageManager(), packageName);
+
+                      visionRequest.getRequestHeaders().set(ANDROID_CERT_HEADER, sig);
+                    }
+                  };
+
+          Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+          builder.setVisionRequestInitializer(requestInitializer);
+
+          Vision vision = builder.build();
+
+          BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                  new BatchAnnotateImagesRequest();
+          batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+            AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+
+            // Add the image
+            Image base64EncodedImage = new Image();
+            // Convert the bitmap to a JPEG
+            // Just in case it's a format that Android understands but Cloud Vision
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+            // Base64 encode the JPEG
+            base64EncodedImage.encodeContent(imageBytes);
+            annotateImageRequest.setImage(base64EncodedImage);
+
+            // add the features we want
+            annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+              Feature labelDetection = new Feature();
+              labelDetection.setType("TEXT_DETECTION");
+              labelDetection.setMaxResults(10);
+              add(labelDetection);
+            }});
+
+            // Add the list of one thing to the request
+            add(annotateImageRequest);
+          }});
+
+          Vision.Images.Annotate annotateRequest =
+                  vision.images().annotate(batchAnnotateImagesRequest);
+          // Due to a bug: requests to Vision API containing large images fail when GZipped.
+          annotateRequest.setDisableGZipContent(true);
+          Log.d(TAG, "created Cloud Vision request object, sending request");
+
+          BatchAnnotateImagesResponse response = annotateRequest.execute();
+          inCloudVisionQueue++;
+          return convertResponseToString(response);
+
+        } catch (GoogleJsonResponseException e) {
+          Log.d(TAG, "failed to make API request because " + e.getContent());
+        } catch (IOException e) {
+          Log.d(TAG, "failed to make API request because of other IOException " +
+                  e.getMessage());
+        }
+        return "Cloud Vision API request failed. Check logs for details.";
+      }
+
+      protected void onPostExecute(String result) {
+        if(result != null) {
+          Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+        }
+        Log.d(TAG, "----------------------------------------");
+        Log.d(TAG, "result:" + result);
+        Log.d(TAG, "----------------------------------------");
+
+      }
+    }.execute();
+  }
+
+  public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+    int originalWidth = bitmap.getWidth();
+    int originalHeight = bitmap.getHeight();
+    int resizedWidth = maxDimension;
+    int resizedHeight = maxDimension;
+
+    if (originalHeight > originalWidth) {
+      resizedHeight = maxDimension;
+      resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+    } else if (originalWidth > originalHeight) {
+      resizedWidth = maxDimension;
+      resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+    } else if (originalHeight == originalWidth) {
+      resizedHeight = maxDimension;
+      resizedWidth = maxDimension;
+    }
+    return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+  }
+
+  private String convertResponseToString(BatchAnnotateImagesResponse response) {
+    String message = "";
+    String number = null;
+    --inCloudVisionQueue; // no need to worry about synchronization. It won't harm.
+
+    List<EntityAnnotation> labels = response.getResponses().get(0).getTextAnnotations();
+    if (labels != null) {
+      for (EntityAnnotation label : labels) {
+        message += String.format(Locale.US, "%.3f: %s", label.getScore(), label.getDescription());
+        number = parseLicensePlate(message);
+        if(number!=null) {
+          return number;
+        }
+        message += " ";
+      }
+    } else {
+      Log.d(TAG,"license plate couldn't be recognized");
+      return null;
+    }
+    return parseLicensePlate(message);
+  }
+
+  String parseLicensePlate(String message) {
+    Log.d(TAG, "Message: " + message);
+    message = message.replaceAll("\\s","");
+    Log.d(TAG, "Formatted Message: " + message);
+
+    Iterator iterator = codes.iterator();
+
+    // if string qualifies to be license plate in India.
+    // this will be removed over the time to put logic at ML to qualify
+    while (iterator.hasNext()){
+      int index = message.indexOf(iterator.next().toString());
+      if(index > -1 && (message.length() >= (index+9))) {
+        String number = message.substring(index, index + 9);
+        char[] num = number.toCharArray();
+
+        if (Character.isDigit(num[2])
+                && Character.isDigit(num[3])
+                && Character.isAlphabetic(num[4])
+  //              && Character.isDigit(num[5])
+  //              && Character.isDigit(num[6])
+  //              && Character.isDigit(num[7])
+  //              && Character.isDigit(num[8])
+                ) {
+          Log.d(TAG, "Number: " + number);
+
+          return number;
+        }
+      }
+    }
+    return null;
   }
 
   @Override
